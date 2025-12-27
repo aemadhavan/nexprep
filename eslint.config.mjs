905c50a84cd31284446a81e3c71750bd03901e1a
Madhavan <enmadahvan@gmail.com>
2025-12-27 14:32:57 +1100

    feat: refactor flashcard retrieval logic and enhance user access validation

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import globals from "globals";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
