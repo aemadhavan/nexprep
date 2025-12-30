c67c8884993724e64a7ba9f5ccd65517bbc8b15b
Madhavan <enmadahvan@gmail.com>
2025-12-30 15:51:30 +1100

    feat: implement cookie consent banner and integrate Google Consent Mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
 import { Toaster as CustomToaster } from "@/components/ui/toaster";
 import { Providers } from "./providers";
+import { ConsentBanner } from "@/components/consent-banner";
 import Script from "next/script";
 import "./globals.css";
 
@@ -30,22 +31,35 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-WB29SPFG');
-          `}
-        </Script>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
           suppressHydrationWarning
         >
+          <Script id="google-consent-mode" strategy="beforeInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+
+              gtag('consent', 'default', {
+                'ad_storage': 'denied',
+                'ad_user_data': 'denied',
+                'ad_personalization': 'denied',
+                'analytics_storage': 'denied'
+              });
+            `}
+          </Script>
+          <Script id="google-tag-manager" strategy="afterInteractive">
+            {`
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MJ43C7TS');
+            `}
+          </Script>
           <noscript>
             <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-WB29SPFG"
+              src="https://www.googletagmanager.com/ns.html?id=GTM-MJ43C7TS"
               height="0"
               width="0"
               style={{ display: 'none', visibility: 'hidden' }}
@@ -55,6 +69,7 @@ export default function RootLayout({
             {children}
             <Toaster position="top-center" richColors />
             <CustomToaster />
+            <ConsentBanner />
           </Providers>
         </body>
       </html>
