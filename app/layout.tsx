f94a2caee2dc552e87893710a3eadb01e59c7ec3
Madhavan <enmadahvan@gmail.com>
2025-12-30 12:51:51 +1100

    feat: replace Google Analytics script with Google Tag Manager for improved tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,22 +30,27 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WB29SPFG');
+          `}
+        </Script>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
           suppressHydrationWarning
         >
-          <Script
-            src="https://www.googletagmanager.com/gtag/js?id=G-SYSZQQRBC6"
-            strategy="afterInteractive"
-          />
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-SYSZQQRBC6');
-            `}
-          </Script>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-WB29SPFG"
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
           <Providers>
             {children}
             <Toaster position="top-center" richColors />
